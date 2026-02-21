d707e511ce9e83d962771075ef38ec82701b1373
Laury PRIN <prinlauryy@gmail.com>
2026-02-21 13:44:14 +0100

    Ajout du Theme.kt

--- app/src/main/java/com/openclassroom/eggtracker/ui/theme/Type.kt
+++ app/src/main/java/com/openclassroom/eggtracker/ui/theme/Type.kt
@@ -1,10 +1,20 @@
 package com.openclassroom.eggtracker.ui.theme
 
+import android.R.attr.fontFamily
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.openclassroom.eggtracker.R
+
+val ChewyFont = FontFamily(
+    Font(
+        R.font.chewy_regular,
+        weight = FontWeight.Normal
+    )
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -14,7 +24,22 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
+    ),
+
+    titleLarge = TextStyle(
+        fontFamily = ChewyFont,
+        fontWeight = FontWeight.Normal,
+        fontSize = 28.sp,
+    ),
+
+    titleMedium = TextStyle(
+        fontFamily = ChewyFont,
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp,
+    ),
+
+
+
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
