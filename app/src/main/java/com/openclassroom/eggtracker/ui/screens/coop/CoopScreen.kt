87835bcc08b09debbb3f349183c468d99ffd2f4e
Laury PRIN <prinlauryy@gmail.com>
2026-03-21 17:50:37 +0100

    Base de l'ecran de gestion des poulailler faite

---
+++ app/src/main/java/com/openclassroom/eggtracker/ui/screens/coop/CoopScreen.kt
@@ -0,0 +1,95 @@
+package com.openclassroom.eggtracker.ui.screens.coop
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.openclassroom.eggtracker.R
+import com.openclassroom.eggtracker.domain.Coop
+
+@Composable
+fun CoopScreen(
+    viewModel: CoopViewModel = hiltViewModel()
+) {
+    // TODO Limitation des poulailler en fonction du statut premium ou non
+    val coops = viewModel.coops.collectAsStateWithLifecycle()
+
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(onClick = { /*TODO*/ }) {
+                Text(text = "+")
+            }
+        }
+    ) { innerPadding ->
+        Box(modifier = Modifier.padding(innerPadding)) {
+            CoopContent(coops.value)
+        }
+    }
+}
+
+@Composable
+fun CoopContent(coops: List<Coop>) {
+    if (coops.isEmpty()) {
+        Box(modifier = Modifier.fillMaxSize().padding(32.dp),
+            contentAlignment = Alignment.Center
+        ){
+            Text(text = stringResource(R.string.no_coops), textAlign = TextAlign.Center, style = MaterialTheme.typography.bodyLarge)
+        }
+    } else {
+        LazyColumn(modifier = Modifier.fillMaxSize()) {
+            coops.forEach { coop ->
+                item {
+                    CoopDisplay(coop)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CoopDisplay(coop: Coop, editCoopButton: () -> Unit = {}, deleteCoopButton: () -> Unit = {}) {
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Text(text = coop.name)
+            Text(text = "${coop.type.labelResId} - ${coop.birdCount}")
+        }
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Button(onClick = { editCoopButton() }) {
+                Text(text = stringResource(R.string.edit_button))
+                // TODO Modifier le poulailler
+            }
+            Button(onClick = { deleteCoopButton() }) {
+                Text(text = stringResource(R.string.delete_button))
+                // TODO Supprimer le poulailler
+            }
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun CoopContentPreview() {
+    CoopContent(emptyList())
+}
+
+
+
+
---
+++ app/src/main/java/com/openclassroom/eggtracker/ui/screens/coop/CoopViewModel.kt
@@ -0,0 +1,18 @@
+package com.openclassroom.eggtracker.ui.screens.coop
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.openclassroom.eggtracker.domain.repository.CoopRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
+import javax.inject.Inject
+
+@HiltViewModel
+class CoopViewModel @Inject constructor(private val coopRepository: CoopRepository) : ViewModel() {
+
+    val coops = coopRepository.getAllCoops()
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), emptyList())
+
+
+}
