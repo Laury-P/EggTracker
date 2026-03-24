e5026df9887692e9cdfee0c07df1b6a4c6a8fc16
Laury PRIN <prinlauryy@gmail.com>
2026-03-24 12:19:16 +0100

    Correction de l'affichage de l'écran AddEditCoopScreen.kt

--- app/src/main/java/com/openclassroom/eggtracker/ui/screens/coop/AddEditCoopScreen.kt
+++ app/src/main/java/com/openclassroom/eggtracker/ui/screens/coop/AddEditCoopScreen.kt
@@ -1,6 +1,8 @@
 package com.openclassroom.eggtracker.ui.screens.coop
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
@@ -14,20 +16,24 @@ import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.openclassroom.eggtracker.R
@@ -39,7 +45,6 @@ import com.openclassroom.eggtracker.ui.theme.EggTrackerTheme
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddEditCoopScreen(viewModel: CoopViewModel = hiltViewModel(), navController: NavController) {
-    // TODO espacé proprement le formulaire
     // TODO gestion de modifier un poulailler
     Scaffold(
         topBar = {
@@ -52,8 +57,12 @@ fun AddEditCoopScreen(viewModel: CoopViewModel = hiltViewModel(), navController:
                             contentDescription = stringResource(R.string.back_button)
                         )
                     }
-                })
-
+                },
+                windowInsets = WindowInsets(0,0,0,0),
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.surface,
+                ),
+            )
         }
     ) { innerPadding ->
         AddEditCoopContent(
@@ -74,12 +83,17 @@ fun AddEditCoopContent(modifier: Modifier = Modifier, onSaveCoopClick: (Coop) ->
     val options = PoultryType.entries.map { it }
     var birdCount by remember { mutableStateOf("") }
 
-    Column(modifier = modifier) {
+    Column(
+        modifier = modifier.fillMaxWidth().padding(horizontal = 16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+        ) {
         OutlinedTextField(
             onValueChange = { name = it },
             label = { Text(stringResource(R.string.coop_name_label)) },
             value = name,
-            singleLine = true
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth()
         )
         ExposedDropdownMenuBox(
             expanded = expanded,
@@ -109,7 +123,7 @@ fun AddEditCoopContent(modifier: Modifier = Modifier, onSaveCoopClick: (Coop) ->
                 }
             }
         }
-        // TODO : regler probleme du 0 quand on rentre un chiffre
+
         OutlinedTextField(
             onValueChange = { newValue -> birdCount = newValue.filter { it.isDigit() } },
             label = { Text(stringResource(R.string.bird_count_label)) },
@@ -123,7 +137,8 @@ fun AddEditCoopContent(modifier: Modifier = Modifier, onSaveCoopClick: (Coop) ->
                 if (birdCount == "0") {
                     Text(text = stringResource(R.string.bird_count_null_error))
                 }
-            }
+            },
+            modifier = Modifier.fillMaxWidth()
         )
 
         Button(
